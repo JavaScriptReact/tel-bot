1db50e344982e6a26f6248192d3c93713bfc8827
LucasCoder1 <lucas.developer.dm@gmail.com>
2021-08-10 16:31:08 +0200

    updated'

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const { createServer } = require("http");
+const express = require("express");
+const cors = require("cors");
 const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = require("./config").TOKEN;
+const PORT = require("./config").PORT;
+
+const app = express();
+const server = createServer(app);
+
+app.use(cors());
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on("message", (msg) => {
   const id = msg.chat.id;
   bot.sendMessage(id, msg.text);
+});
+
+server.listen(PORT, () => {
+  console.log("Server is active");
 });
